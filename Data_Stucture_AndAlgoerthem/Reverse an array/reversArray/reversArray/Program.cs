80a1d41c39deaf9b50e7eaec898631e304e16c22
Tasneem-Alabed <tsneemabedd@gmail.com>
2023-06-24 17:55:38 +0300

    Add project files.

---
+++ Data_Stucture_AndAlgoerthem/Reverse an array/reversArray/reversArray/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace reversArray
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] arr1=new int[2];
+            revers( arr1);
+        }
+        public static Array revers(int[] arr1)
+        {
+            if (arr1.Length == 0)
+            {
+                return null;
+            }
+            else if (arr1.Length == 1)
+            {
+                return arr1;
+            }
+            else if (arr1.Length >= 2)
+            {
+                int[] arr2 = new int[arr1.Length];
+                for (int i = arr1.Length - 1; i >= 0; i--)
+                {
+                    arr2[arr1.Length - i - 1] = arr1[i];
+                }
+                return arr2;
+            }
+            return arr1;
+        }
+    }
+}
