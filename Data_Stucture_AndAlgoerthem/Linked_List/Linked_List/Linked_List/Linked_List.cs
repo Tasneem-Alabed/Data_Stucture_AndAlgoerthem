a24731237b059a585e4349978c5d4cddfdeb3b3c
Tasneem <117952461+Tasneem-Alabed@users.noreply.github.com>
2023-07-09 02:43:08 +0300

    Delete Data_Stucture_AndAlgoerthem/array-binary-search/linked_list directory

--- Data_Stucture_AndAlgoerthem/Linked_List/Linked_List/Linked_List/Linked_List.cs
+++ Data_Stucture_AndAlgoerthem/Linked_List/Linked_List/Linked_List/Linked_List.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Linq;
@@ -78,6 +79,181 @@ namespace linked_List
 
         }
 
+        public void append(int value)
+        {
+            Node node = new Node(value);
+            Node pointer = new Node();
+            pointer = head;
+            if(head == null)
+            {
+                head = tail = node;
+            }
+            while (pointer != null)
+            {
+                if (pointer.Next == null)
+                {
+
+                    pointer = node;
+                    tail = node;
+
+                }
+                pointer = pointer.Next;
+            }
+        }
+        public void insert_before(int value, int newValue)
+        {
+            Node node = new Node(newValue);
+            Node pointer;
+            pointer = head;
+            while (pointer != null)
+            {
+                if (pointer.Data == value)
+                {
+                    if (pointer == head)
+                    {
+                        node.Next = pointer;
+                        head = node;
+
+
+                    }
+                    else
+                    {
+                        Node pointer2;
+                        pointer2 = head;
+                        while (pointer2.Next.Next != pointer)
+                        {
+                            if (pointer2.Next == pointer)
+                            {
+                                node.Next = pointer;
+                                pointer2.Next = node;
+                            }
+                        }
+                    }
+                    break;
+
+                }
+                pointer = pointer.Next;
+
+
+
+            }
+            Console.WriteLine("No change, method exception");
+        }
+
+        public void Initial_List(int value, int newValue)
+        {
+            Node node = new Node(newValue);
+            Node pointer;
+            pointer = head;
+            while (pointer != null)
+            {
+                if (pointer.Data == value)
+                {
+                    node.Next = pointer.Next;
+                    pointer.Next = node;
+                    break;
+                }
+                pointer = pointer.Next;
+            }
+            Console.WriteLine("No change, method exception");
+        }
+
+        public int kthFromEnd(int k)
+        {
+            try
+            {
+                int counter = 0;
+                Node node;
+                node = head;
+                while (node != null)
+                {
+                    counter++;
+                    node = node.Next;
+                    if (head == tail && k == 0)
+                    {
+                        return head.Data;
+                    }
+                    if (node.Next == null)
+                    {
+
+                        if (counter  == k)
+                        {
+                            return head.Data;
+                        }
+                        if (k < counter && k >= 0)
+                        {
+
+                            Node value;
+                            value = head;
+                            int leght = counter - k + 1;
+                            for (int i = 1; i < leght; i++)
+                            {
+
+                                value = value.Next;
+                                if (i == leght - 1)
+                                {
+                                    return value.Data;
+                                }
+
+                            }
+                        }
+                        break;
+                    }
+
+
+
+
+                }
+
+            }
+
+
+            catch (IndexOutOfRangeException e)
+            {
+                throw new IndexOutOfRangeException();
+
+
+            }
+
+            throw new IndexOutOfRangeException();
+        }
+        public LinkedList zipLists(LinkedList list1 , LinkedList list2) {
+            LinkedList z = new LinkedList();
+            try
+            {
+                
+                Node pointer1 = new Node();
+                pointer1 =list1.head;
+                Node pointer2 = new Node();
+                pointer2 = list2.head;
+               
+                while (pointer1 != null || pointer2 != null)
+                {
+                  
+                    if (list1 != null)
+                    {
+                      
+                        z.append(pointer1.Data);
+                        pointer1 = pointer1.Next;
+                        break;
+
+                    }
+                    if (list2 != null)
+                    {
+                        z.append(pointer2.Data);
+                        pointer2 = pointer2.Next;
+                        break;
+                    }
+
+                }
+
+                return z;
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                throw e;
+            }
+        }
 
     }
 }
--- Data_Stucture_AndAlgoerthem/Linked_List/Linked_List/ListUnitTest/UnitTest1.cs
+++ Data_Stucture_AndAlgoerthem/Linked_List/Linked_List/ListUnitTest/UnitTest1.cs
@@ -7,6 +7,9 @@ namespace TestProject1
     public class UnitTest1
     {
         LinkedList s = new LinkedList();
+        LinkedList b = new LinkedList();
+        LinkedList test = new LinkedList();
+
         [Fact]
         public void test1()
         {
@@ -94,5 +97,153 @@ namespace TestProject1
 
 
         }
+        [Fact]
+        public void test8()
+        {
+
+            s.insert(99);
+            s.insert(3);
+            s.insert(10);
+            s.append(5);
+            Assert.Equal(5, s.tail.Data);
+          
+        }
+        [Fact]
+        public void test9()
+        {
+
+            s.insert(99);
+            s.insert(3);
+            s.insert(10);
+            s.append(5);
+            s.append(51);
+            Assert.Equal(51, s.tail.Data);
+
+        }
+        [Fact]
+        public void test10()
+        {
+
+            s.insert(99);
+            s.insert(3);
+            s.insert(10);
+            
+            s.insert_before(10, 77);
+            Assert.Equal(77, s.head.Data);
+
+        }
+        [Fact]
+        public void test11()
+        {
+
+            s.insert(99);
+            s.insert(3);
+            s.insert(10);
+
+            s.insert_before(10, 77);
+            Assert.Equal(77, s.head.Data);
+
+        }
+        [Fact]
+        public void test12()
+        {
+
+            s.insert(99);
+            s.insert(3);
+            s.insert(10);
+
+            s.Initial_List(99, 77);
+            Assert.Equal(77, s.tail.Next.Data);
+
+        }
+        [Fact]
+        public void test13()
+        {
+
+            s.insert(99);
+            s.insert(3);
+            s.insert(10);
+
+          
+            Assert.Throws<IndexOutOfRangeException>(() => s.kthFromEnd(-1));
+
+        }
+    
+    [Fact]
+    public void test14()
+    {
+
+        s.insert(99);
+        s.insert(3);
+        s.insert(10);
+
+
+        Assert.Throws<IndexOutOfRangeException>(() => s.kthFromEnd(51));
+
+    }
+        [Fact]
+        public void test15()
+        {
+
+            s.insert(99);
+            s.insert(3);
+            s.insert(10);
+
+
+            Assert.Equal(3, s.kthFromEnd(1));
+
+        }
+        [Fact]
+        public void test16()
+        {
+
+            s.insert(99);
+            s.insert(3);
+            s.insert(10);
+
+
+            Assert.Equal(99, s.kthFromEnd(0));
+
+        }
+        [Fact]
+        public void test17()
+        {
+
+            s.insert(99);
+            s.insert(3);
+            s.insert(10);
+
+
+            Assert.Equal(10, s.kthFromEnd(2));
+
+        }
+        [Fact]
+        public void zipLists()
+        {
+            LinkedList s = new LinkedList();
+            LinkedList b = new LinkedList();
+            LinkedList test = new LinkedList();
+
+            s.insert(99);
+            s.insert(3);
+            s.insert(10);
+
+            b.insert(11);
+            b.insert(77);
+            b.insert(8);
+
+            test.insert(10);
+            test.insert(8);
+            test.insert(3);
+            test.insert(77);
+            test.insert(99);
+            test.insert(11);
+
+            
+            Assert.Equal(test, s.zipLists(s,b));
+
+        }
+      
+
     }
 }
