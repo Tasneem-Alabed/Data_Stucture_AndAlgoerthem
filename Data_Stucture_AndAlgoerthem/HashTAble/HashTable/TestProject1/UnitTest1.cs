ad95cbe1715a2c0aac93e5cb2a8090c5be4fab92
Tasneem <tsneemabedd@gmail.com>
2023-10-02 01:57:40 +0300

    solve

--- Data_Stucture_AndAlgoerthem/HashTAble/HashTable/TestProject1/UnitTest1.cs
+++ Data_Stucture_AndAlgoerthem/HashTAble/HashTable/TestProject1/UnitTest1.cs
@@ -4,6 +4,64 @@ namespace HashTable.Tests
 {
     public class TestProject1
     {
+        [Fact]
+        public void TestSetAndGetKeyValue()
+        {
+             
+            var hashTable = new Hash(5);
 
+             
+            hashTable.set("key1", "value1");
+            hashTable.set("key2", "value2");
+
+             
+            Assert.Equal("value1", hashTable.get("key1"));
+            Assert.Equal("value2", hashTable.get("key2"));
+        }
+
+        [Fact]
+        public void TestNotFound()
+        {
+             
+            var hashTable = new Hash(5);
+ 
+            Assert.Equal("Not Found", hashTable.get("nonexistent_key"));
+        }
+
+        [Fact]
+        public void TestHasExistingKey()
+        {
+           
+            var hashTable = new Hash(5);
+            hashTable.set("existing_key", "value");
+
+            Assert.True(hashTable.has("existing_key"));
+        }
+
+        [Fact]
+        public void TestHasNonExistentKey()
+        {
+           
+            var hashTable = new Hash(10);
+
+            
+            Assert.False(hashTable.has("nonexistent_key"));
+        }
+
+        [Fact]
+        public void TestReturnAllKeys()
+        {
+            
+            var hashTable = new Hash(5);
+            hashTable.set("key1", "value1");
+            hashTable.set("key2", "value2");
+
+           
+            string[] keys = hashTable.Keys();
+
+           
+            Assert.Contains("key1", keys);
+            Assert.Contains("key2", keys);
+        }
     }
 }
