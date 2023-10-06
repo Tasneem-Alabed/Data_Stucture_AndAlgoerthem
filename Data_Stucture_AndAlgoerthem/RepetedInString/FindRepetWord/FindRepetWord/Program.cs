4d817bb8df68b2baa450c0065876e02c7ac5dd66
Tasneem <tsneemabedd@gmail.com>
2023-10-06 15:28:38 +0300

    done

--- Data_Stucture_AndAlgoerthem/RepetedInString/FindRepetWord/FindRepetWord/Program.cs
+++ Data_Stucture_AndAlgoerthem/RepetedInString/FindRepetWord/FindRepetWord/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Diagnostics.Metrics;
+using System;
 
 namespace FindRepetWord
 {
@@ -7,37 +8,187 @@ namespace FindRepetWord
     {
         static void Main(string[] args)
         {
-           
+            string word1 = "It was the best of times, it was the worst of times, it was the age of wisdom,";
+
+            Hash n = new Hash(word1.Length);
+           Console.WriteLine(n.repeatedword(word1));
         }
 
-        public static string repeated_word(string Book)
+    }
+    public class Node<T>
         {
-           if (string.IsNullOrEmpty(Book))
+            public T Value { get; set; }
+
+            public Node<T> Next { get; set; }
+
+            public Node(T value)
             {
-                return Book;
+                Value = value;
             }
 
-            else
+        }
+
+        public class LinkedList<T>
+        {
+            public LinkedList()
             {
-                string [] words = Book.Split(" "); 
-                for(int couter =0; couter < words.Length; couter++) 
+            }
+
+            public Node<T> head { get; set; }
+
+            public LinkedList(T value)
+            {
+                Node<T> node = new Node<T>(value);
+                head = node;
+            }
+
+            public void insert(T value)
+            {
+                Node<T> node = new Node<T>(value);
+
+                node.Next = head;
+                head = node;
+            }
+        }
+
+        public class Hash
+        {
+            private LinkedList<KeyValuePair<string, string>>[] map { get; set; }
+            public Hash(int size)
+            {
+                map = new LinkedList<KeyValuePair<string, string>>[size];
+            }
+
+            public int hash(string key)
+            {
+                int number = 0;
+
+                char[] arr = key.ToCharArray();
+                for (int i = 0; i < arr.Length; i++)
                 {
-                    string s1= words[couter];
-                    for(int i = couter + 1; i < words.Length-1; i++)
+                    number += arr[i];
+                    number = (number * 599) % map.Length;
+                }
+                return number;
+
+            }
+            public void set(string key, string value)
+            {
+                int number = hash(key);
+                if (map[number] == null)
+                {
+                    map[number] = new LinkedList<KeyValuePair<string, string>>();
+                }
+
+                KeyValuePair<string, string> Value = new KeyValuePair<string, string>(key, value);
+
+                map[number].insert(Value);
+            }
+
+            public string get(string key)
+            {
+                for (int i = 0; i < map.Length; i++)
+                {
+                    if (map[i] != null)
                     {
-                        string s2 = words[i];
-                   var a=  s1?.Equals(s2,StringComparison.CurrentCultureIgnoreCase);
-                        if(a==true)
+                        Node<KeyValuePair<string, string>> current = map[i].head;
+                        while (current != null)
                         {
-                            return s1;
-                        }                       
+                            if (key == current.Value.Key)
+                            {
+                                return current.Value.Value;
+                            }
+                            current = current.Next;
+                        }
+                    }
+                }
+                return "Not Found";
+            }
+
+            public bool has(string key)
+            {
+                for (int i = 0; i < map.Length; i++)
+                {
+                    if (map[i] != null)
+                    {
+                        Node<KeyValuePair<string, string>> current = map[i].head;
+                        while (key != current.Value.Key)
+                        {
+                            current = current.Next;
+                        }
+                        if (key == current.Value.Key)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+                return false;
+            }
+
+            public string[] Keys()
+            {
+                List<string> keyList = new List<string>();
+
+                for (int i = 0; i < map.Length; i++)
+                {
+                    Node<KeyValuePair<string, string>> current = map[i]?.head;
+
+                    while (current != null)
+                    {
+                        keyList.Add(current.Value.Key);
+                        current = current.Next;
                     }
                 }
 
+                return keyList.ToArray();
             }
-           return "";
+
+        public Hash splits(string paregraph)
+        {
+            var hashsplit = new Hash(paregraph.Length);
+            string[] n = paregraph.Split(" ");
+           for(int i = 0; i < n.Length; i++)
+            {
+                
+                hashsplit.set(n[i], n[i]);
+            }
+
+           return hashsplit;
+
         }
+        public string repeatedword(string word)
+        {
+            var hashsplit = splits(word); 
+            //string result;
+
+            for (int i = 0; i < hashsplit.map.Length; i++)
+            {
+                Node<KeyValuePair<string, string>> current = hashsplit.map[i]?.head;
+
+                while (current != null)
+                {
+                    Node<KeyValuePair<string, string>> prevous = current.Next;
+                    while (prevous != null)
+                    {
+                        var a = current.Value.Value.Equals(prevous.Value.Value, StringComparison.CurrentCultureIgnoreCase);
+                        if (a == true)
+                        {
+                            return current.Value.Value;
+                        }
+                        prevous = prevous.Next; 
+                    }
+                    current = current.Next;
+                }
+            }
+            return "No repeated Word";
+        }
+
     }
 
 }
+
+
+
+
 
--- Data_Stucture_AndAlgoerthem/RepetedInString/FindRepetWord/TestFindWord/UnitTest1.cs
+++ Data_Stucture_AndAlgoerthem/RepetedInString/FindRepetWord/TestFindWord/UnitTest1.cs
@@ -4,62 +4,28 @@ namespace TestFindWord
     public class UnitTest1
     {
         [Fact]
-        public void EmptyString()
+        public void Repeatedword()
         {
-            string word = "";
-             
-            var result = FindRepetWord.Program.repeated_word(word);
+           
+            var hash = new Hash(10);
 
-            Assert.Equal(word, result);
+          
+            string repeatedWord = hash.repeatedword("This is a test. This  only a test.");
 
-        }
-
-        [Fact]
-        public void testfindrepeted()
-        {
-            string word = "Once upon a time, there was a brave princess who...";
-
-            var result = FindRepetWord.Program.repeated_word(word);
-
-            Assert.Equal("a", result);
-
-        }
-
-        [Fact]
-        public void testfindrepeted1()
-        {
-            string word = "It was the best of times, it was the worst of times, it was the age of wisdom," +
-                " it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity," +
-                " it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, " +
-                "we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other" +
-                " way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, " +
-                "for good or for evil, in the superlative degree of comparison only...";
-
-            var result = FindRepetWord.Program.repeated_word(word);
-
-            Assert.Equal("It", result);
-
+           
+            Assert.Equal("This", repeatedWord);
         }
         [Fact]
-        public void testfindrepeted2()
+        public void NoRepeatedword()
         {
-            string word = "It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York...";
 
-            var result = FindRepetWord.Program.repeated_word(word);
+            var hash = new Hash(10);
 
-            Assert.Equal("was", result);
 
-        }
+            string repeatedWord = hash.repeatedword("This is a test");
 
-        [Fact]
-        public void TestNoRepeted()
-        {
-            string word = "This code written to find the repeted word";
 
-            var result = FindRepetWord.Program.repeated_word(word);
-
-            Assert.Equal("", result);
-
+            Assert.Equal("No repeated Word", repeatedWord);
         }
     }
 }
